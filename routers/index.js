cf65b7d1e038435c7400ef521cfaec3a5b05e7aa
Sebastian G <orjuela.desarrollador@gmail.com>
2024-05-09 15:05:14 -0500

    Implementando peticiones post

--- routers/index.js
+++ routers/index.js
@@ -4,7 +4,7 @@ const servicios1 = require('./servicios1.router');
 
 module.exports = (app) => {
     const router =  express.Router();
-    app.use("api/v1", router);
-    router.use("servicios1", servicios1);
-    app.use("servicios1", servicios1);
+    app.use("/api/v1", router);
+    router.use("/servicios1", servicios1);
+    app.use("/servicios1", servicios1);
 }
--- routers/servicios1.router.js
+++ routers/servicios1.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const faker = require("faker");
 
-const router =express.Router();
+const router = express.Router();
 
 router.get("/msg/:id/:code", (req, res)=> {
     //Params en ruta
@@ -10,6 +10,20 @@ router.get("/msg/:id/:code", (req, res)=> {
 
     //Query params en ruta
     const { msg } = req.query;
+
+    res.json({
+        msg: msg, 
+        id : id,
+        code: code,
+        name: faker.commerce.productName(),
+        price: parseFloat(faker.commerce.price(), 10),
+        image: faker.image.imageUrl()
+    });
+});
+
+
+router.post("/msg/", (req, res)=> {
+    const {id, msg, code } = req.body;
 
     res.json({
         msg: msg, 
