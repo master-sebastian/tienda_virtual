6ca316614a746fb2659a6ab4707380fd2ddc0cd1
Sebastian G <orjuela.desarrollador@gmail.com>
2024-05-09 14:54:25 -0500

    Organizando rutas de servicio 1 en grupos

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,10 @@
+const express = require('express');
+
 const servicios1 = require('./servicios1.router');
 
 module.exports = (app) => {
-    app.use("/servicios1", servicios1)
+    const router =  express.Router();
+    app.use("api/v1", router);
+    router.use("servicios1", servicios1);
+    app.use("servicios1", servicios1);
 }
