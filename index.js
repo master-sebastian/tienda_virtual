3aa7c2f5d812450b42a4e8dc506425557b2fbdfc
Sebastian G <orjuela.desarrollador@gmail.com>
2024-05-09 14:04:27 -0500

    Instalacion de expressjs

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hola mundo")
+const express = require('express');
+const app = express();
+const port =  3000;
+
+app.get("/", (req, res)=> {
+    res.send("Servicio inicial")
+})
+
+app.listen(port, ()=>{
+    console.log("Server, port"+port)
+})
